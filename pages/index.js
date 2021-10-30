77f754cdd50de291d18c47e5c19dcdb2e737e2d0
Heli Sivunen <heli@mooncat.tv>
2021-10-30 21:44:32 +0300

    change heading, import chacra elements into index page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { Heading, Link, Text, Code, Flex, Box, Center } from "@chakra-ui/react"
 
 export default function Home() {
   return (
@@ -13,7 +14,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Colour-changing app!
         </h1>
 
         <p className={styles.description}>
